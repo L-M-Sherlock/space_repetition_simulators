0396d5824f5e1b68b2b8fa76e750f16fa75ea9b8
L-M-Sherlock <jarrett.ye@outlook.com>
2021-04-07 18:00:32 +0800

    增加数据生成与记录功能

--- SIncSimulator.py
+++ SIncSimulator.py
@@ -3,11 +3,13 @@ import random
 from collections import deque
 from scipy import optimize
 import matplotlib.pyplot as plt
+import pandas as pd
+import time
 
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
-start_stability = 3
+start_stability = 1
 a = -16
 b = 0.23
 
@@ -18,6 +20,7 @@ class Item:
         self.SInc = 0
         self.R = 0
         self.feedback = 0
+        self.used_ivl = 0
         self.S = start_stability
         self.last_review = -1
         self.ivl_history = []
@@ -28,10 +31,11 @@ class Item:
         self.__calculate_r(date)
         self.feedback = 1 if random.random() < self.R else 0
         self.fb_history.append(self.feedback)
-        self.ivl_history.append(0 if self.last_review < 0 else date - self.last_review)
+        self.used_ivl = 0 if self.last_review < 0 else date - self.last_review
+        self.ivl_history.append(self.used_ivl)
         if self.feedback == 0:
             self.lapse += 1
-            self.S = max(0.5, start_stability * np.power(0.8, self.lapse))
+            self.S = start_stability
         else:
             self.__calculate_sinc()
             self.__update_s()
@@ -44,6 +48,7 @@ class Item:
             self.R = 0.9
 
     def __calculate_sinc(self):
+        # self.SInc = - a * np.power(self.S, -b) * (np.exp(1 - self.R) - 1) * 1.1 + 1
         self.SInc = a * np.power(self.S, -b) * np.log(self.R) + 1
 
     def __update_s(self):
@@ -56,45 +61,59 @@ class Item:
         def esinc(x):
             return - x * (a * np.power(self.S, -b) * np.log(x) + 1)
 
-        r = optimize.fminbound(esinc, 0, 1)
+        def esinc2(x):
+            return - x * (- 1.1 * a * np.power(self.S, -b) * (np.exp(1 - x) - 1) + 1)
+
+        r = optimize.fminbound(esinc2, 0, 1)
         return int(round(max(1, np.log(r) / np.log(0.9) * self.S), 0))
 
 
 if __name__ == '__main__':
 
-    learn_days = 500  # 模拟时长
-    deck_size = 6000  # 新卡片总量
+    learn_days = 600  # 模拟时长
+    deck_size = 1000000  # 新卡片总量
     items_all = [Item() for _ in range(0, deck_size)]
     items = deque(items_all)
     item_per_day = [[] for _ in range(0, learn_days)]
-    card_per_day_limit = 100  # 每日学习总上限
+    card_per_day_limit = 100000  # 每日学习总上限
+    new_card_per_day_limit = 1000
     card_per_day = [{'forget': 0, 'recall': 0, 'new': 0} for i in range(0, learn_days)]
     end_day = 0
-    forget_index = 0.2  # 遗忘比率
-    period_len = 30  # 滚动平均区间
+    forget_index = 0.1  # 遗忘比率
+    period_len = 200  # 滚动平均区间
     new_card_per_day = np.array([0.0] * learn_days)
     new_card_per_day_average_per_period = np.array([0.0] * learn_days)
     workload_per_day = np.array([0.0] * learn_days)
     workload_per_day_average_per_period = np.array([0.0] * learn_days)
+    revlog = np.empty(shape=[0, 5], dtype=np.object)
 
     for day in range(0, learn_days):
-        card_per_day[day]['new'] = max(0, card_per_day_limit - len(item_per_day[day])) if len(items) > 0 else 0
-        while len(item_per_day[day]) < card_per_day_limit and len(items) > 0:
+        card_per_day[day]['new'] = min(max(0, card_per_day_limit - len(item_per_day[day])),
+                                       new_card_per_day_limit) if len(items) > 0 else 0
+        i = 0
+        while len(item_per_day[day]) < card_per_day_limit and len(items) > 0 and i < new_card_per_day_limit:
             item_per_day[day].append(items.pop())
+            i += 1
 
         new_card_per_day[day] = card_per_day[day]['new']
 
         if day >= period_len:
-            new_card_per_day_average_per_period[day] = np.true_divide(new_card_per_day[day - period_len:day + 1].sum(),
+            new_card_per_day_average_per_period[day] = np.true_divide(new_card_per_day[day - period_len:day].sum(),
                                                                       period_len)
         else:
             new_card_per_day_average_per_period[day] = np.true_divide(new_card_per_day[:day + 1].sum(), day + 1)
 
         if len(items) > 0:
             end_day = day
-        for item in item_per_day[day]:
+        today_revlog = np.empty(shape=[len(item_per_day[day]), 5], dtype=np.object)
+        for idx, item in enumerate(item_per_day[day]):
+            ivl_history = str(item.ivl_history)[1:-1]
+            fb_history = str(item.fb_history)[1:-1]
             item.review(day)
-            ivl = item.interval_threshold(1 - forget_index)
+            today_revlog[idx] = [ivl_history, fb_history, item.used_ivl, item.feedback, item.R]
+            forget_index = random.triangular(0.05, 0.2, 0.1)
+            ivl = int(round(item.interval_threshold(1 - forget_index)))
+            # ivl = int(round(ivl) * 5 * round(random.uniform(0.15, 1.04), 1))
             # ivl = item.interval_max_esinc()
             delay = 0
             while day + ivl + delay < learn_days and len(item_per_day[day + ivl + delay]) >= card_per_day_limit:
@@ -103,20 +122,23 @@ if __name__ == '__main__':
                 continue
             item_per_day[day + ivl + delay].append(item)
             if item.feedback == 0:
-                card_per_day[day + ivl]['forget'] += 1
+                card_per_day[day + ivl + delay]['forget'] += 1
             else:
-                card_per_day[day + ivl]['recall'] += 1
-    recall = 0
-    for item in items_all:
-        item.review(learn_days)
-        if item.feedback == 1:
-            recall += 1
-
-    print("learn all item day:", end_day)
-    print("recall in learn_days", recall)
+                card_per_day[day + ivl + delay]['recall'] += 1
+        revlog = np.concatenate((revlog, today_revlog))
+    # recall = 0
+    # for item in items_all:
+    #     if item.last_review > 0:
+    #         item.review(learn_days + 1)
+    #         recall += item.R
+    print("数据条数", len(revlog))
+    result = pd.DataFrame(revlog, columns=["ivl_history", "fb_history", "used_ivl", "feedback", "R"])
+    result.to_csv(f"revlog{int(time.time())}.tsv", sep="\t", index=False)
+    total_learned = int(sum(new_card_per_day))
+    print("learn all item day:", end_day + 1)
     plt.plot(new_card_per_day_average_per_period)
     plt.xlabel("时间/天")
-    plt.ylabel(f"每日新卡片数量({period_len}天平均)")
-    plt.title(f"{learn_days}天/遗忘比例{forget_index:.2f}")
+    plt.ylabel(f"每日新卡片数量{period_len}天平均)")
+    # plt.title(f"{learn_days}天/遗忘比例{forget_index:.2f}/总学习量{total_learned}/记忆保留总量{int(recall)}")
     plt.grid(True)
     plt.show()
