d1b15c5b274cb6a38d06bd56e8ffdb034cc6e53d
L-M-Sherlock <jarrett.ye@outlook.com>
2021-04-07 18:00:44 +0800

    增加数据可视化功能

---
+++ furget_curve_visualization.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pylab as plt
+from matplotlib.pyplot import MultipleLocator
+
+sns.set(style="white")
+sns.set(font='SimHei')
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+plt.rcParams['figure.figsize'] = (12.0, 4.0)
+plt.rcParams['figure.dpi'] = 300
+
+
+def _compute_bin_p_recall(group):
+    return pd.DataFrame([
+        {'r': group['feedback'].mean()}
+    ])
+
+
+def _compute_bin_r(group):
+    return pd.DataFrame([
+        {'R': group['R'].mean()}
+    ])
+
+
+def _compute_bin_count(group):
+    return pd.DataFrame([
+        {'count': len(group)}
+    ])
+
+
+if __name__ == '__main__':
+    df = pd.read_csv("revlog1617713653.tsv", sep="\t", keep_default_na=False)
+    fb_group = df.groupby(["fb_history"])
+
+    for fb_history, fb_batch in fb_group:
+        if len(fb_history) > 12 or len(fb_history) < 2 or '0' in fb_history or len(fb_batch) < 10000:
+            continue
+
+        global_count = fb_batch.groupby(
+            fb_batch['used_ivl']
+        ).apply(_compute_bin_count).reset_index().drop(['level_1'], axis=1)[:40]
+        # if min(global_count['count']) < 1000:
+        #     continue
+        global_forget_curve = fb_batch.groupby(
+            fb_batch['used_ivl']
+        ).apply(_compute_bin_p_recall).reset_index().drop(['level_1'], axis=1)[:40]
+        global_R = fb_batch.groupby(
+            fb_batch['used_ivl']
+        ).apply(_compute_bin_r).reset_index().drop(['level_1'], axis=1)[:40]
+        plt.figure()
+        title = f"反馈序列[{fb_history}]_数据量{len(fb_batch)}"
+        plt.title(title)
+        plt.xlabel('used_ivl')
+        plt.ylabel('count')
+        plt.bar(global_count['used_ivl'], global_count['count'], alpha=0.2)
+        plt.xticks(fb_batch['used_ivl'].drop_duplicates().sort_values(), rotation=-90)
+        plt.twinx()
+        plt.plot(global_R['used_ivl'], global_R['R'], 'g*-')
+        plt.plot(global_forget_curve['used_ivl'], global_forget_curve['r'], 'r*-')
+        plt.ylim([0.5, 1.0])
+        plt.ylabel('R')
+        plt.savefig('./plot/' + title + '.jpg')
+        plt.close()
+        # plt.show()
+        # continue
+
+        ivl_group = fb_batch.groupby(["ivl_history"])
+        for ivl_history, ivl_batch in ivl_group:
+            if len(ivl_batch) < 1000:
+                continue
+            global_count = ivl_batch.groupby(
+                ivl_batch['used_ivl']
+            ).apply(_compute_bin_count).reset_index().drop(['level_1'], axis=1)[:40]
+            global_forget_curve = ivl_batch.groupby(
+                ivl_batch['used_ivl']
+            ).apply(_compute_bin_p_recall).reset_index().drop(['level_1'], axis=1)[:40]
+            global_R = ivl_batch.groupby(
+                ivl_batch['used_ivl']
+            ).apply(_compute_bin_r).reset_index().drop(['level_1'], axis=1)[:40]
+            plt.figure()
+            title = f"反馈序列[{fb_history}]_间隔序列[{ivl_history}]_数据量{len(ivl_batch)}"
+            plt.title(title)
+            plt.xlabel('used_ivl')
+            plt.ylabel('count')
+            plt.bar(global_count['used_ivl'], global_count['count'], alpha=0.2)
+            plt.xticks(ivl_batch['used_ivl'].drop_duplicates().sort_values())
+            plt.twinx()
+            plt.plot(global_R['used_ivl'], global_R['R'], 'g*-')
+            plt.plot(global_forget_curve['used_ivl'], global_forget_curve['r'], 'r*-')
+            plt.ylim([0.5, 1.0])
+            plt.ylabel('R')
+            # plt.show()
+            plt.savefig('./plot/' + title + '.jpg')
+            plt.close()
