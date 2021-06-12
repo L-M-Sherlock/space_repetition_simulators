fc4893c9ec8661e7a22a3fbfbe032028b084e747
L-M-Sherlock <jarrett.ye@outlook.com>
2021-06-12 16:38:13 +0800

    期望稳定性增长

--- StateSpace.py
+++ StateSpace.py
@@ -18,16 +18,27 @@ def cal_SInc_linear(S, R):
 
 if __name__ == "__main__":
     sinc_log_matrix = np.empty(shape=[10, 10])
+    esinc_log_matrix = np.empty(shape=[10, 10])
     for i in range(1, 11):
         for j in range(1, 11):
-            sinc_log_matrix[i - 1][j - 1] = cal_SInc_log(np.power(2, j), i / 10)
+            s = np.power(2, j)
+            r = i / 10
+            sinc_log_matrix[i - 1][j - 1] = cal_SInc_log(s, r)
+            esinc_log_matrix[i - 1][j - 1] = sinc_log_matrix[i - 1][j - 1] * r + 1 / s * (1 - r)
     sinc_exp_matrix = np.empty(shape=[10, 10])
+    esinc_exp_matrix = np.empty(shape=[10, 10])
     for i in range(1, 11):
         for j in range(1, 11):
-            sinc_exp_matrix[i - 1][j - 1] = cal_SInc_exp(np.power(2, j), i / 10)
+            s = np.power(2, j)
+            r = i / 10
+            sinc_exp_matrix[i - 1][j - 1] = cal_SInc_exp(s, r)
+            esinc_exp_matrix[i - 1][j - 1] = sinc_exp_matrix[i - 1][j - 1] * r + 1 / s * (1 - r)
     sinc_linear_matrix = np.empty(shape=[10, 10])
+    esinc_linear_matrix = np.empty(shape=[10, 10])
     for i in range(1, 11):
         for j in range(1, 11):
-            sinc_linear_matrix[i - 1][j - 1] = cal_SInc_linear(np.power(2, j), i / 10)
-
+            s = np.power(2, j)
+            r = i / 10
+            sinc_linear_matrix[i - 1][j - 1] = cal_SInc_linear(s, r)
+            esinc_linear_matrix[i - 1][j - 1] = sinc_linear_matrix[i - 1][j - 1] * r + 1 / s * (1 - r)
     print("end")
