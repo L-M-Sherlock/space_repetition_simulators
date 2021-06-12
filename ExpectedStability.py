9c289189e02261c1e547023b4c146e37332f148f
L-M-Sherlock <jarrett.ye@outlook.com>
2021-06-12 19:42:06 +0800

    优化重复次数累计方式

--- ExpectedStability.py
+++ ExpectedStability.py
@@ -1,12 +1,11 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 plt.rcParams['figure.figsize'] = (8.0, 4.0)
 plt.rcParams['figure.dpi'] = 300
-
 
 start_stability = 1
 a = -16
@@ -23,12 +22,11 @@ def stability(reps, r):
 def estability(reps, r):
     es = start_stability
     for k in range(reps):
-        es = np.power(r, k + 1) * stability(k+1, r) + (1 - r) * es
+        es = np.power(r, k + 1) * stability(k + 1, r) + (1 - r) * es
     return es
 
 
-if __name__ == "__main__":
-    random.seed(114)
+def matrix():
     estability_matrix = np.empty(shape=[10, 9])
     for i in range(1, 11):
         for j in range(1, 10):
@@ -36,30 +34,70 @@ if __name__ == "__main__":
             r = j / 10
             estability_matrix[i - 1][j - 1] = estability(reps, r)
     print(estability_matrix)
-    recalls = [0.9, 0.8, 0.6, 0.4, 0.3]
+
+
+def diff_recall():
+    num_samples = 10000
+    day_long = 1000
+    recalls = [0.95, 0.9, 0.8, 0.6, 0.2]
     for recall in recalls:
-        repeats = [list() for _ in range(10000)]
-        for _ in range(10000):
+        repeats = np.array([0.0 for _ in range(day_long)])
+        for _ in range(num_samples):
             day = 0
             s = 1
-            reps = 0
-            while day < 10000:
+            while day < day_long:
+                ivl = round(s)
+                for i in range(ivl):
+                    if i + day >= day_long:
+                        break
+                    repeats[day + i] += 1 / ivl
                 if random.random() < recall:
                     s *= a * np.power(s, -b) * np.log(recall) + 1
                 else:
                     s = np.log(recall) / np.log(0.9)
-                repeats[day].append(reps)
-                reps += 1
-                day += round(s)
-        avg_repeats = []
-        for repeat in repeats:
-            if len(repeat) > 0:
-                avg_repeats.append(sum(repeat)/len(repeat))
-            else:
-                avg_repeats.append(0)
-        plt.plot(savgol_filter(avg_repeats, 999, 1), "-", label=f'保留率{recall}')
-
+                day += ivl
+        repeats = repeats / num_samples
+        for i in range(day_long - 1):
+            repeats[i + 1] += repeats[i]
+        plt.plot(repeats, "-", label=f'保留率{recall}')
     plt.grid(True)
     plt.xlabel("时间/天")
     plt.legend()
     plt.show()
+
+
+def diff_start():
+    num_samples = 10000
+    day_long = 1000
+    starts = [1, 10, 100, 1000]
+    recall = 0.9
+    for start in starts:
+        repeats = np.array([0.0 for _ in range(day_long)])
+        for _ in range(num_samples):
+            day = 0
+            s = start
+            while day < day_long:
+                ivl = round(s)
+                for i in range(ivl):
+                    if i + day >= day_long:
+                        break
+                    repeats[day + i] += 1 / ivl
+                if random.random() < recall:
+                    s *= a * np.power(s, -b) * np.log(recall) + 1
+                else:
+                    s = np.log(recall) / np.log(0.9)
+                day += ivl
+        repeats = repeats / num_samples
+        for i in range(day_long - 1):
+            repeats[i + 1] += repeats[i]
+        plt.plot(repeats, "-", label=f'初始稳定性{start}')
+    plt.grid(True)
+    plt.xlabel("时间/天")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    matrix()
+    diff_recall()
+    diff_start()
