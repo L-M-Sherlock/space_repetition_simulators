49af397b7005cabb2f6bcc9c4a278c5427405311
L-M-Sherlock <jarrett.ye@outlook.com>
2021-06-16 00:17:03 +0800

    求差定常量

--- ReviewStress.py
+++ ReviewStress.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
 from init import *
+import math
 
 
 def sinc(s, r):
@@ -33,8 +35,8 @@ def opt_stress(r):
 
 def diff_recall():
     num_samples = 10000
-    day_long = 1000
-    recalls = [0.95, 0.9, 0.8, 0.6, 0.2]
+    day_long = 2000
+    recalls = [0.9, 0.8, 0.6, 0.2]
     for recall in recalls:
         repeats = np.array([0.0 for _ in range(day_long)])
         for _ in range(num_samples):
@@ -63,31 +65,41 @@ def diff_recall():
 
 def diff_start():
     num_samples = 10000
-    day_long = 1000
-    starts = [1, 10, 100, 1000]
+    day_long = 3000
+    starts = [1, 2, 4, 8]
     recall = 0.9
+    repeats = []
     for start in starts:
-        repeats = np.array([0.0 for _ in range(day_long)])
+        repeats.append(np.array([0.0 for _ in range(day_long)]))
         for _ in range(num_samples):
             day = 0
             s = start
             while day < day_long:
                 ivl = round(s)
+                day += ivl
                 for i in range(ivl):
-                    if i + day >= day_long:
+                    if day >= day_long:
                         break
-                    repeats[day + i] += 1 / ivl
+                    repeats[-1][day - i - 1] += 1 / ivl
                 if random.random() < recall:
                     s *= a * np.power(s, -b) * np.log(recall) + 1
                 else:
                     s = np.log(recall) / np.log(0.9)
-                day += ivl
-        repeats = repeats / num_samples
+        repeats[-1] = repeats[-1] / num_samples
         for i in range(day_long - 1):
-            repeats[i + 1] += repeats[i]
-        plt.plot(repeats, "-", label=f'初始稳定性{start}')
+            repeats[-1][i + 1] += repeats[-1][i]
+        plt.figure(1)
+        plt.plot(repeats[-1], "-", label=f'初始稳定性{start}')
     plt.grid(True)
     plt.xlabel("时间/天")
+    plt.legend()
+    plt.figure(2)
+    plt.grid(True)
+    plt.xlabel("时间/天")
+    for i in range(len(starts) - 1):
+        result = repeats[0] - repeats[i+1]
+        mean = sum(result) / len(result)
+        plt.plot(result, label=f'初始稳定性{starts[i+1]}-均值{mean:.2f}')
     plt.legend()
     plt.show()
 
@@ -97,5 +109,5 @@ if __name__ == "__main__":
     print(r1)
     r2 = optimize.fminbound(opt_stress, 0, 1)
     print(r2)
-    diff_recall()
+    # diff_recall()
     diff_start()
